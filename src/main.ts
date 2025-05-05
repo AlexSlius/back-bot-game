8b2f208b566d0ef1579eac2e7f582c5324833825
Oleksandr Sliusarchuk <o.sliusarchuk@heavensoft.com.ua>
2025-05-05 18:42:58 +0300

    s

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
   app.enableCors({
     origin: ['http://localhost:3000', 'http://85.217.170.47', 'https://85.217.170.47'],
