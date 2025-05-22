839d548f6b996efa1584e0d0d36bf584c1aa1413
Oleksandr Sliusarchuk <o.sliusarchuk@heavensoft.com.ua>
2025-05-22 14:36:59 +0300

    s

--- src/common/services/mail.service.ts
+++ src/common/services/mail.service.ts
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 export class SendEmail {
     async send({ email = '', isNew = false, password = '' }: { email: string, isNew?: boolean, password: string }) {
         await transporter.sendMail({
-            from: `"СРМ МГ" <${process.env.MAIN_FORM}>`,
+            from: `"CRM MG" <${process.env.MAIN_FORM}>`,
             to: email,
             subject: isNew ? 'Ласкаво просимо! Ваш пароль' : 'Ваш пароль',
             html: this.htmlText(isNew, password)
@@ -22,17 +22,18 @@ export class SendEmail {
 
     htmlText(isNew: Boolean, password: string) {
         return `
-        <div style="font-family: Arial, sans-serif; padding: 20px;">
-            <h2 style="color: #3858e9;">${isNew ? "Вітаємо у нашій системі!" : "Підтримка СРМ МГ"}</h2>
-            <p>${isNew ? "Дякуємо за реєстрацію." : ""}</p>
-            <p><strong>Ваш ${isNew ? "" : "новий"} пароль для входу:</strong></p>
-            <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px; display: inline-block;">
-                <strong>${password}</strong>
+            <div style="font-family: Arial, sans-serif; padding: 20px;">
+                <h2 style="color: #3858e9;">${isNew ? "Вітаємо у нашій системі!" : "Підтримка CRM MG"}</h2>
+                <a href="https://crm.mindgame.ua/">https://crm.mindgame.ua/</a>
+                <p>${isNew ? "Дякуємо за реєстрацію." : ""}</p>
+                <p><strong>Ваш ${isNew ? "" : "новий"} пароль для входу:</strong></p>
+                <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px; display: inline-block;">
+                    <strong>${password}</strong>
+                </div>
+                <p>Будь ласка, збережіть його у безпечному місці.</p>
+                <br/>
+                <p>З найкращими побажаннями,<br/>Команда підтримки</p>
             </div>
-            <p>Будь ласка, збережіть його у безпечному місці.</p>
-            <br/>
-            <p>З найкращими побажаннями,<br/>Команда підтримки</p>
-        </div>
-    `
+        `
     }
 }
